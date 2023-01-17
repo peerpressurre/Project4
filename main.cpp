d880aa24e414f7ef3a5d393eb582bcee61f3ac25
student <student@PC-G-04>
2023-01-17 18:19:04 +0200

    1w

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int switch_on;
+    std::cout << "When said enter '1' for first exercise->";
+    std::cout << "When said enter '2' for first exercise->";
+    std::cout << "When said enter '3' for first exercise->";
+    std::cout << "When said enter '4' for first exercise->";
+    std::cout << "Enter switch_on";
+    std::cin >> switch_on;
+    switch (switch_on)
+    {
+    default:
+        break;
+    }
+    return 0;
+}
